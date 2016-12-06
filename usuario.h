030f7ddb7ab6d05a4a41b7a8046cb33adb4ed3bb
Felipe da Costa Malaquias <felipedf7@gmail.com>
2016-12-06 12:05:39 -0200

    Update usuario.h

--- usuario.h
+++ usuario.h
@@ -3,8 +3,11 @@
 #include <string>
 #include "social.h"
 
-const int MAX_USUARIOS = 100;
-
+/*************************************************************************** 
+* Classe: Usuario
+* Descrição
+* Contem toda a estrutura basica de um usuario do programa
+***************************************************************************/
 class Usuario
 {
 private:
@@ -20,6 +23,23 @@ private:
 
 
 public:
+	/*************************************************************************** 
+	* Metodo: Construtor da classe Usuario
+	* Descrição
+	* Usada para criar uma instancia da classe Usuario
+	* Parametros
+	* nIdade - Idade do usuario, deve ser um numero maior que 0
+	* nGenero - Genero do usuario, deve ser m (homem), f (mulher), o (outro)
+	* nId - Id do novo usuario
+	* nNome - Nome do usuario, deve ser uma string nao vazia e que nao contenha numeros
+	* nInteresses - vetor de inteiros contendo os interesses do usuario
+	* nCEP - CEP do usuario
+	* nEscolaridade - Contem um inteiro representando a escolaridade do usuario
+	* Asseriva de entrada 
+	*		nIdade > 0
+	*		nGenero == 'm' || nGenero == 'f' || nGenero == 'o'
+	*		nNome.size() > 0
+	***************************************************************************/
 	Usuario(int nIdade, int nId, char nGenero, std::string nNome, std::vector<int> nInteresses, std::string nCEP, int nEscolaridade);
 	~Usuario();
 
@@ -40,9 +60,24 @@ public:
 	void set_escolaridade(int nova_escolaridade);
 	void set_nome(std::string novo_nome);
 	void set_cep(std::string novo_cep);
-
 	void novos_interesses(std::vector<int> nInteresses);
+	
+	/*************************************************************************** 
+	* Metodo: Adiciona interesse
+	* Descrição
+	* 	Adiciona um novo interesse no vetor de enderecos
+	* Assertivas de entrada
+	* 	nInteresse deve ser equivalente a um interesse valido e nao existir na lista
+	***************************************************************************/
 	void add_interesse(int nInteresse);
+	
+	/*************************************************************************** 
+	* Metodo: Deleta interesse
+	* Descrição
+	* 	Deleta um interesse da lista de interesses
+	* Assertivas de entrada
+	* 	nInteresse deve ser equivalente a um interesse valido e nao existir na lista
+	***************************************************************************/
 	void delete_interesse(int nInterese);
 
 	void bakuhatsu();
